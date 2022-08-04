0bc76a12b561613601174ff5f7bc5ae98df2084f
Gerardo Carileo Acuña <gerardo.catrileo@gmail.com>
2022-08-04 17:10:43 -0400

    Creacion del proyectoo
    Primer commit

---
+++ sge/src/test/java/cl/falabella/sge/aslplus/sge/SgeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.falabella.sge.aslplus.sge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
