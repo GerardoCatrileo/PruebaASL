84148d1a2a6f3b64ef161e978c790475bc1d5d69
hz9-developer <68916041+hz9-developer@users.noreply.github.com>
2022-08-08 11:42:02 -0400

    Se agrega propiedad xml

---
+++ Ocenc.java
@@ -0,0 +1,59 @@
+package cl.falabella.sge.aslplus.sge.entity;
+
+import java.beans.JavaBean;
+import java.io.Serializable;
+
+@JavaBean
+public class Ocenc implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Long numOC;
+	public String negocio;
+	public String canal;
+	private String xml;
+	
+	public Ocenc() {
+		
+	}
+	
+	
+	public String getNegocio() {
+		return negocio;
+	}
+
+	public void setNegocio(String negocio) {
+		this.negocio = negocio;
+	}
+
+	public String getCanal() {
+		return canal;
+	}
+
+	public void setCanal(String canal) {
+		this.canal = canal;
+	}
+
+
+	public Long getNumOC() {
+		return numOC;
+	}
+
+	public void setNumOC(Long numOC) {
+		this.numOC = numOC;
+	}
+
+
+	public String getXml() {
+		return xml;
+	}
+
+
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+	
+	
+	
+	
+}
